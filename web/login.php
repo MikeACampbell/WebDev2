8c633c933b8b2fc53d220f41aa0a740a41ae1d40
MikeACampbell <mikecampbella@gmail.com>
2017-05-27 16:58:38 -0500

    updated

--- web/login.php
+++ web/login.php
@@ -3,13 +3,13 @@
    $dbUrl = getenv('DATABASE_URL');
 
   $dbopts = parse_url($dbUrl);
-  //print "<p>$dbUrl</p>\n\n";
+
   $dbHost = $dbopts["host"];
   $dbPort = $dbopts["port"];
   $dbUser = $dbopts["user"];
   $dbPassword = $dbopts["pass"];
   $dbName = ltrim($dbopts["path"],'/');
-  //print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
   
   try {
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
@@ -18,12 +18,12 @@
     print "<p>error: $ex->getMessage() </p>\n\n";
     die();
   }
- /*
+ 
       // username and password sent from form 
       
       $myusername = $_POST['username'];
       $mypassword = $_POST['password']; 
-      
+ /*     
       $stmt = prepare('SELECT id FROM userList WHERE username = '$myusername' and pword = '$mypassword'');
       $result = $stmt->execute();
 	  
